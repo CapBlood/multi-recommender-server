e1bf8cef259d8c12e713d119ecd181364ed67d53
CapBlood <stalker.anonim@mail.ru>
2022-12-24 17:17:59 +0300

    fix: исправлена инициализация БД

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,4 +1,6 @@
 set -e
 
 mongod --fork --logpath /var/log/mongod.log
-poetry run python3 ./scripts/init_db/load_anime_csv_mongo.py
+poetry run dvc pull
+# poetry run python3 ./scripts/init_db/load_anime_csv_mongo.py
+poetry run dvc repro
