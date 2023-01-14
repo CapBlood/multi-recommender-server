663cf96f7e49e478db23ed9d0afe81102f4d7f8b
CapBlood <stalker.anonim@mail.ru>
2023-01-14 20:19:18 +0300

    some changes

--- hybrid_rs/server/server.py
+++ hybrid_rs/server/server.py
@@ -7,19 +7,49 @@ from whitenoise import WhiteNoise
 from hybrid_rs.server.django_setup import setup
 
 
-def get_server_app() -> WSGIHandler:
+def get_server_app(debug=False) -> WSGIHandler:
     setup()
     application = get_wsgi_application()
-    application = WhiteNoise(
-        application,
-        root="hybrid_rs/server")
+    if debug:
+        application = WhiteNoise(
+            application,
+            autorefresh=True,
+            root="hybrid_rs/server")
+    else:
+        application = WhiteNoise(
+            application,
+            root="hybrid_rs/server")
     application.add_files("hybrid_rs/server", prefix="static/")
     return application
 
 
 def run_server():
     subprocess.run(
-        ['gunicorn', '-c', 'hybrid_rs/server/gunicorn_config.py', 'hybrid_rs.server.server:get_server_app()'],
+        [
+            'gunicorn', 
+            '-c', 
+            'hybrid_rs/server/gunicorn_config.py',
+            'hybrid_rs.server.server:get_server_app()'
+        ],
+    )
+
+def run_debug():
+    import os
+
+    env = {
+        **os.environ,
+        'DEBUG': '1'
+    }
+    
+    subprocess.run(
+        [
+            'gunicorn',
+            '--reload',
+            '-c',
+            'hybrid_rs/server/gunicorn_config.py',
+            'hybrid_rs.server.server:get_server_app(debug=True)'
+        ],
+        env=env
     )
 
 def manager() -> None:
